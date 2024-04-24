2e8d1932e4743a9e4ffd6c0e3c4aa1d4fc09082b
Samuel <samisred169@gmail.com>
2024-04-23 23:38:09 -0400

    added gunicorn and rtxt

--- repositories/db.py
+++ repositories/db.py
@@ -3,11 +3,10 @@ from psycopg_pool import ConnectionPool
 
 pool = None
 
-
 def get_pool():
     global pool
     if pool is None:
         pool = ConnectionPool(
-            conninfo=os.getenv('DB_CONNECTION_STRING', ''),
+            conninfo=os.getenv('DATABASE_URL', ''),
         )
     return pool
